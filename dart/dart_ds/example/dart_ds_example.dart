2df7adcae66e8202d7e537cc22569f0fb7e022aa
shadyaziza <shadyaziza@gmail.com>
2022-06-09 10:17:00 +0200

    init dart project

---
+++ dart/dart_ds/example/dart_ds_example.dart
@@ -0,0 +1,6 @@
+import 'package:dart_ds/dart_ds.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ dart/dart_ds/lib/dart_ds.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dart_ds;
+
+export 'src/dart_ds_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
