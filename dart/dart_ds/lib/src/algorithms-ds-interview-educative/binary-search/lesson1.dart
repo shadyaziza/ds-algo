5592a2f8c73c60a6cba9106499c3c3a0da9ba70a
shadyaziza <shadyaziza@gmail.com>
2022-06-09 10:55:00 +0200

    dart/algorithms-educative/1,1

---
+++ dart/dart_ds/lib/src/algorithms-ds-interview-educative/binary-search/lesson1.dart
@@ -0,0 +1,16 @@
+int binarySearch(List<int> arr, int target) {
+  int left = 0;
+  int right = arr.length - 1;
+
+  while (left <= right) {
+    int mid = (left + right) ~/ 2;
+
+    if (arr[mid] == target) return arr[mid];
+    if (arr[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
+}
